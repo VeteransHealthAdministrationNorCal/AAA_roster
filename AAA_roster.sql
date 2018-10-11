aaea30370f1d5e0283a08fafd57970846d127652
Harry L. George IV <37850370+InfoSac@users.noreply.github.com>
2018-10-11 09:09:41 -0700

    Update AAA_roster.sql

--- AAA_roster.sql
+++ AAA_roster.sql
@@ -1,61 +1,28 @@
-IF OBJECT_ID('tempdb..#rawData') IS NOT NULL 
-
-DROP TABLE
-  #rawData
-
 SELECT 
-  patient.PatientSSN,
-  patient.PatientSID,
-  patient.PatientFirstName,
-  patient.PatientLastName,
-  StopCode.StopCodeName,
-  convert(varchar(10),VisitDateTime,101) AS VisitDate,
-  PrimaryStopCodeSID,
-  SecondaryStopCodeSID
-
-INTO
-  #rawData
-
+	SPatient.PatientSSN,
+	COUNT(SPatient.PatientSSN) AS OutpatientEncounterInYear
 FROM
-  LSV.BISL_R1VX.AR3Y_Outpat_Visit AS visit
-INNER JOIN LSV.dim.StopCode AS StopCode 
-  ON visit.PrimaryStopCodeSID = StopCode.StopCodeSID
-  AND visit.Sta3n = StopCode.Sta3n
-INNER JOIN LSV.BISL_R1VX.AR3Y_SPatient_SPatient AS patient
-  ON patient.PatientSID = visit.PatientSID
-
+	LSV.BISL_R1VX.AR3Y_SPatient_SPatient AS SPatient
+	INNER JOIN LSV.BISL_R1VX.AR3Y_Outpat_Visit AS Visit
+		ON SPatient.PatientSID = Visit.PatientSID
+		AND SPatient.Sta3n = Visit.Sta3n
+	LEFT JOIN LSV.Dim.StopCode AS DimStopCode
+		ON Visit.PrimaryStopCodeSID = DimStopCode.StopCodeSID
+		AND Visit.Sta3n = DimStopCode.Sta3n
+	INNER JOIN LSV.Outpat.Problemlist AS ProblemList 
+		ON Visit.PatientSID = ProblemList.PatientSID
+		AND Visit.Sta3n = ProblemList.Sta3n
+	INNER JOIN LSV.Dim.ICD10 AS DimICD10
+		ON ProblemList.ICD10SID = DimICD10.ICD10SID
+		AND ProblemList.Sta3n = DimICD10.Sta3n
 WHERE
-  visit.Sta3n = '612'
-  AND StopCode.StopCode = '415'
-  AND visit.VisitDateTime >= CONVERT(Datetime2(0), DATEADD(YEAR, -1, GETDATE()))
-  AND Patient.PatientLastName not like 'ZZ%'
-  AND Patient.DeceasedFlag != 'Y'
+	SPatient.Sta3n = '612'
+	AND DimStopCode.StopCode = '415'
+	AND DimICD10.ICD10Code LIKE '%I71.[4,9]%'
+	AND Visit.VisitDateTime >= CONVERT(Datetime2(0), DATEADD(YEAR, -1, GETDATE()))
+	AND SPatient.PatientName not like 'ZZ%'
+	AND SPatient.DeceasedFlag != 'Y'
+GROUP BY
+	SPatient.PatientSSN
 ORDER BY
-  patient.PatientSSN
-
-SELECT
-  a.PatientSSN,
-  COUNT(a.PatientSSN) AS OutpatientEncounterInYear
-
-FROM (
-  SELECT DISTINCT
-    t.PatientSSN,
-    t.PatientFirstName,
-    t.PatientLastName,
-    t.VisitDate,
-    icd10.ICD10code
-
-  FROM
-    #rawData AS t
-    INNER JOIN LSV.Outpat.Problemlist AS pbl 
-      ON pbl.PatientSID = t.PatientSID
-    INNER JOIN LSV.dim.ICD10 AS icd10
-      ON icd10.ICD10SID = pbl.ICD10SID
-  
-  WHERE
-    icd10.ICD10Code LIKE '%I71.[4,9]%'
-     ) AS a
-  GROUP BY
-    a.PatientSSN
-  ORDER BY
-    a.PatientSSN
+	SPatient.PatientSSN
